99efa09ccb31ef1b3552a3e47d986124278982c0
fede1312 <124668768+fede1312@users.noreply.github.com>
2023-02-07 12:52:34 +0100

    Add files via upload

---
+++ fede.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[100];
+    char b[100];
+    printf("inserisci due nomi: \n");
+    scanf("%s %s", a, b);
+    //char sonostupide = a, b;
+    printf("%s e %s sono stupide\n", a, b);
+}
