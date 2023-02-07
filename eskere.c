3ce87f138aac7fff3c571111617ed47b0ff9858c
fede1312 <124668768+fede1312@users.noreply.github.com>
2023-02-07 11:17:57 +0100

    Add files via upload

---
+++ eskere.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    printf("inserisci 2 numeri: \n");
+    scanf("%d %d", &a, &b);
+    int divisione =  a / b;
+    printf("divisione era: %d\n", divisione);
+
+
+}
