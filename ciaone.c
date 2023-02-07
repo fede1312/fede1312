305d2559342f4500d34c584e555ff8de12ef9a5f
fede1312 <124668768+fede1312@users.noreply.github.com>
2023-02-07 10:27:22 +0100

    Add files via upload

---
+++ ciaone.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    
+    printf("ínserisci un numero\n");
+    scanf("%d", &x);
+    printf("x: %d\n", x);
+    return (0);
+}
---
+++ fedefika.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+   int numero = 35;
+   double numeroogrande = 268236917;
+   float numeropiccollo = 0,15;
+   char effe = 35;
+
+   printf("numero: %d\n, numero")
+   printf(numeroogrande: %f\n, numeroogrande")
+   printf("numeropiccollo: %f\n, numeropiccollo")
+   printf()
+
