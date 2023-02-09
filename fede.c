35bf4761d2bcee87df430329e93500636e2e5bde
fede1312 <124668768+fede1312@users.noreply.github.com>
2023-02-09 11:48:26 +0100

    Add files via upload

--- fede.c
+++ fede.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-
-int main()
+int main ()
 {
-    char a[100];
-    char b[100];
-    printf("inserisci due nomi: \n");
-    scanf("%s %s", a, b);
-    //char sonostupide = a, b;
-    printf("%s e %s sono stupide\n", a, b);
+    int x;
+    int y;
+    int z = 1;
+    printf("inserisci 2 numeri\n");
+    scanf("%d %d", &x, &y);
+    while(y>0)
+    {
+z = x * z;
+y = y - 1;
+    }
+    printf ("potenza %d\n", z);
 }
